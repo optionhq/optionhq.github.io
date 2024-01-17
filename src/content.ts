409a419a60cd92ac594af44f4038bee9bf956e94
William Fischer <william.fischer42@gmail.com>
2024-01-17 08:00:57 -0600

    updates mailto

--- src/content.ts
+++ src/content.ts
@@ -4,7 +4,7 @@ export const content = {
   gameCTA: 'Play the Game',
   gameURL: 'https://www.negationgame.com',
   mailCTA: 'Contact Us',
-  mailto: 'will@veeo.io',
+  mailto: 'cnnr.mccrmck@gmail.com',
   bodyMd: 'body',
   disc: `Negation Game is currently in "open alpha." We're constantly experimenting and iterating
           to make it easy, delightful, and useful.`,
